14984e58925a1c75f2af7195136dcd3d0d45d386
des-afari <afaridesmond@gmail.com>
2023-03-12 15:23:26 +0000

    created base model class

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Base Model Script"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base Model Class"""
+    def __init__(self):
+        """Gets called when you create an instance"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of a class"""
+        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
+
+    def save(self):
+        """Update updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all items of __dict__"""
+        base_model_dict = self.__dict__
+
+        base_model_dict['__class__'] = self.__class__.__name__
+        base_model_dict['created_at'] = self.created_at.isoformat()
+        base_model_dict['updated_at'] = self.updated_at.isoformat()
+
+        return base_model_dict
--- tests/test_base_models.py
+++ tests/test_base_models.py
@@ -1,10 +1,47 @@
+#!/usr/bin/python3
+"""Unittest for BaseModel Class"""
 import unittest
+from datetime import datetime
+import time
+from models.base_model import BaseModel
 
 
-class TestFoo(unittest.TestCase):
-    def test_foo(self):
-        self.assertTrue(True)
+class TestBaseModel(unittest.TestCase):
+    """BaseModel Test Class"""
+    def setUp(self):
+        """Setup method"""
+        self.b = BaseModel()
 
+    def tearDown(self):
+        """Teardown method"""
+        del self.b
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_docstring(self):
+        """Test docstring of the file"""
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_is_instance(self):
+        """Test if object is instance of class"""
+        self.assertIsInstance(self.b, BaseModel)
+        self.assertIsInstance(self.b.created_at, datetime)
+        self.assertIsInstance(self.b.updated_at, datetime)
+
+    def test_id(self):
+        """Test id"""
+        self.assertTrue(hasattr(self.b, "id"))
+        self.assertEqual(type(self.b.id), str)
+
+    def test_save(self):
+        """Test save method"""
+        self.assertEqual(self.b.created_at, self.b.created_at)
+        time.sleep(5)
+        self.b.save()
+        self.assertNotEqual(self.b.created_at, self.b.updated_at)
+
+    def test_to_dict(self):
+        """Test to_dict method"""
+        self.assertEqual(self.b.to_dict()['__class__'], 'BaseModel')
